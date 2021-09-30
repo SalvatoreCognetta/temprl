bb813b28d127b6aeda5f7c7456b554d909d77658
SalvatoreCognetta <salvatore.cognetta@gmail.com>
2021-09-30 13:28:04 +0200

    Some changes to Synthetic class, to be completed.

--- temprl/wrapper.py
+++ temprl/wrapper.py
@@ -143,42 +143,33 @@ class TemporalGoalWrapperSynthetic(gym.Wrapper):
     def __init__(
         self,
         env: gym.Env,
-        temp_goals: List[TemporalGoal],
-        fluent_extractor: FluentExtractor,
+        automaton: DFA
     ):
         """
         Wrap a Gym environment with a temporal goal.
 
         :param env: the Gym environment to wrap.
-        :param temp_goals: the temporal goal to be learnt
-        :param fluent_extractor: the extractor of the fluents.
-          A callable that takes in input an observation and the last action
-          taken, and returns the set of fluents true in the current state.
+        :param automaton: the DFA automaton.
         """
         super().__init__(env)
-        self.temp_goals = temp_goals
-        self.fluent_extractor: FluentExtractor = fluent_extractor
-        self.observation_space = self._get_observation_space()
-
-    def _get_observation_space(self) -> gym.spaces.Space:
-        """Return the observation space."""
-        temp_goals_shape = tuple(tg.observation_space.n for tg in self.temp_goals)
-        return GymTuple((self.env.observation_space, MultiDiscrete(temp_goals_shape)))
+        self.state = 0 # Initial state
+        self.automaton = automaton
+        self.transitions = automaton.get_transitions()
 
     def step(self, action):
         """Do a step in the Gym environment."""
         obs, reward, done, info = super().step(action)
-        fluents = self.fluent_extractor(obs, action)
-        states_and_rewards = [tg.step(fluents) for tg in self.temp_goals]
-        next_automata_states, temp_goal_rewards = zip(*states_and_rewards)
-        total_goal_rewards = sum(temp_goal_rewards)
+
+        # next_automata_states = possible states from self.state taken from transitions
+        
+
         obs_prime = (obs, next_automata_states)
-        reward_prime = reward + total_goal_rewards
+        reward_prime = reward + 0
         return obs_prime, reward_prime, done, info
 
     def reset(self, **_kwargs):
         """Reset the Gym environment."""
         obs = super().reset()
-        fluents = self.fluent_extractor(obs, None)
-        automata_states = [tg.reset(fluents) for tg in self.temp_goals]
-        return obs, automata_states
+        # fluents = self.fluent_extractor(obs, None)
+        # automata_states = [tg.reset(fluents) for tg in self.temp_goals]
+        return obs, 0
